1f2099f8c3d416ddc7bce894970c4dd7fe9d3310
mark odhiambo okoth <markokoth96@gmail.com>
2020-04-07 18:20:35 +0300

    beautiful drawer in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('TikTok'),
+          title: Text('cardMind'),
           backgroundColor: Colors.purpleAccent,
         ),
         drawer: Drawer(
